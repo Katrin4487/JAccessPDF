639f2f6f66efb4dfaeda98df65e2fd04a895b9ba
FLOPPY\kstal <k.stallberg@gmail.com>
2025-07-08 23:29:02 +0200

    Add constructors and child context method to StyleResolverContext
    Introduced constructors to initialize StyleResolverContext

--- model/src/main/java/de/kaiser/model/style/StyleResolverContext.java
+++ model/src/main/java/de/kaiser/model/style/StyleResolverContext.java
@@ -1,4 +1,39 @@
 package de.kaiser.model.style;
 
+import java.util.Map;
+
 public class StyleResolverContext {
+
+    private final Map<String, ElementStyle> styleMap;
+    private final TextBlockStyleProperties defaultBlockStyle;
+    private final TextBlockStyleProperties parentBlockStyle;
+
+    // --- Constructor ---
+
+    public StyleResolverContext(Map<String, ElementStyle> styleMap, TextBlockStyleProperties defaultBlockStyle) {
+        this(styleMap, defaultBlockStyle, defaultBlockStyle); // parent style is default
+    }
+
+    private StyleResolverContext(Map<String, ElementStyle> styleMap, TextBlockStyleProperties defaultBlockStyle, TextBlockStyleProperties parentBlockStyle) {
+        this.styleMap = styleMap;
+        this.defaultBlockStyle = defaultBlockStyle;
+        this.parentBlockStyle = parentBlockStyle;
+    }
+
+    /**
+     * Creates a new context for the child element. The parent style will set on it
+     */
+    public StyleResolverContext createChildContext(TextBlockStyleProperties newParentBlockStyle) {
+        return new StyleResolverContext(this.styleMap, this.defaultBlockStyle, newParentBlockStyle);
+    }
+
+    // --- Getter ---
+
+    public Map<String, ElementStyle> getStyleMap() {
+        return styleMap;
+    }
+
+    public TextBlockStyleProperties getParentBlockStyle() {
+        return parentBlockStyle;
+    }
 }
