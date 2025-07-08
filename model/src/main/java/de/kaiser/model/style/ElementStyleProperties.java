7dda7fb84319673d2ca36b8507c46dc8942ae789
FLOPPY\kstal <k.stallberg@gmail.com>
2025-07-08 22:10:50 +0200

    Add style model framework with extensible property support (ElementStyle, -Property is ready)

--- model/src/main/java/de/kaiser/model/style/ElementStyleProperties.java
+++ model/src/main/java/de/kaiser/model/style/ElementStyleProperties.java
@@ -1,4 +1,29 @@
 package de.kaiser.model.style;
 
-public class ElementStyleProperties {
+/**
+ * Abstract base class for all style property sets.
+ * It defines the essential contract for style inheritance and copying,
+ * which all concrete style property classes must implement.
+ */
+public abstract class ElementStyleProperties {
+
+    /**
+     * Merges the properties from a base style into this style.
+     * Typically, properties from the base style are only applied if they are
+     * not already set in this style.
+     *
+     * @param base The base style to inherit from. Can be null.
+     */
+    public abstract void mergeWith(ElementStyleProperties base);
+
+    /**
+     * Creates a deep copy of this style properties object.
+     * This is crucial to prevent style modifications on one element
+     * from affecting another.
+     *
+     * @return A new instance with the same property values as this object.
+     */
+    public abstract ElementStyleProperties copy();
+
+    public abstract String getFontStyleName();
 }
---
+++ model/src/main/java/de/kaiser/model/style/StyleTargetTypes.java
@@ -0,0 +1,20 @@
+package de.kaiser.model.style;
+
+/**
+ * Defines the available 'target-element' constants for style definitions.
+ * Using these constants prevents typos in the style JSON configuration.
+ */
+public final class StyleTargetTypes {
+
+    /** Prevents instantiation of the class. */
+    private StyleTargetTypes() {}
+
+    public static final String PARAGRAPH = "paragraph";
+    public static final String HEADLINE = "headline";
+    public static final String LIST = "list";
+    public static final String TABLE = "table";
+    public static final String TABLE_CELL = "table-cell";
+    public static final String SECTION = "section";
+    public static final String TEXT_RUN = "text-run";
+}
+
