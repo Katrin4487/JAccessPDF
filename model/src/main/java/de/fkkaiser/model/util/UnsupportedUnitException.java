ee1a5e160eb2f2801ffde3ddc13190418ec5ca12
katrin <k.stallberg@gmail.com>
2025-11-30 08:12:06 +0100

    Copyright in header for Section und DimensionUtil

--- model/src/main/java/de/fkkaiser/model/util/UnsupportedUnitException.java
+++
@@ -1,7 +0,0 @@
-package de.fkkaiser.model.util;
-
-public class UnsupportedUnitException extends RuntimeException {
-    public UnsupportedUnitException(String message) {
-        super(message);
-    }
-}
