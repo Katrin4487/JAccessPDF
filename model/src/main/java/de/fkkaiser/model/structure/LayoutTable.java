7c2e402a9ddd489338d4c106591850115224a110
katrin <k.stallberg@gmail.com>
2025-12-16 21:52:11 +0100

    java doc for layout table

--- model/src/main/java/de/fkkaiser/model/structure/LayoutTable.java
+++ model/src/main/java/de/fkkaiser/model/structure/LayoutTable.java
@@ -17,6 +17,7 @@ package de.fkkaiser.model.structure;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import de.fkkaiser.model.JsonPropertyName;
+import de.fkkaiser.model.annotation.Internal;
 import de.fkkaiser.model.style.ElementBlockStyleProperties;
 import de.fkkaiser.model.style.ElementStyle;
 import de.fkkaiser.model.style.LayoutTableStyleProperties;
@@ -27,6 +28,14 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+/**
+ * Represents a layout table element in a document structure.
+ * A layout table contains two elements: one on the left and one on the right.
+ * It is used for arranging content side by side (structural layout).
+ *
+ * @author Katrin Kaiser
+ * @version 1.0.1
+ */
 public class LayoutTable implements Element{
 
     private final String styleClass;
@@ -36,6 +45,13 @@ public class LayoutTable implements Element{
     private LayoutTableStyleProperties resolvedStyle;
 
 
+    /**
+     * Constructor for creating a LayoutTable object.
+     *
+     * @param styleClass  The style class of the layout table.
+     * @param elementLeft The element to be placed on the left side.
+     * @param elementRight The element to be placed on the right side.
+     */
     public LayoutTable(
             @JsonProperty(JsonPropertyName.STYLE_CLASS) String styleClass,
             @JsonProperty(JsonPropertyName.ELEMENT_LEFT) Element elementLeft,
@@ -47,27 +63,63 @@ public class LayoutTable implements Element{
 
     }
 
+    /**
+     * Gets the left element of the layout table.
+     *
+     * @return The left element.
+     */
+    @Internal
     public Element getElementLeft() {
         return elementLeft;
     }
 
+    /**
+     * Gets the right element of the layout table.
+     *
+     * @return The right element.
+     */
+    @Internal
     public Element getElementRight() {
         return elementRight;
     }
 
+    /**
+     * Gets the resolved style properties for the layout table.
+     *
+     * @return The resolved layout table style properties.
+     */
+    @Internal
     public LayoutTableStyleProperties getResolvedStyle() {
         return resolvedStyle;
     }
 
+    /**
+     * Sets the resolved style properties for the layout table.
+     *
+     * @param resolvedStyle The resolved layout table style properties to set.
+     */
+    @Internal
     public void setResolvedStyle(LayoutTableStyleProperties resolvedStyle) {
         this.resolvedStyle = resolvedStyle;
     }
 
+    /**
+     * Gets the type of the element.
+     *
+     * @return The element target type.
+     */
+    @Internal
     @Override
     public ElementTargetType getType() {
         return ElementTargetType.LAYOUT_TABLE;
     }
 
+    /**
+     * Gets the style class of the layout table.
+     *
+     * @return The style class.
+     */
+    @Internal
     @Override
     public String getStyleClass() {
         return styleClass;
@@ -78,6 +130,7 @@ public class LayoutTable implements Element{
      *
      * @param context The style resolver context containing style map and default text style properties
      */
+    @Internal
     @Override
     public void resolveStyles(StyleResolverContext context) {
 
